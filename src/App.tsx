e8d7b9f3c1031f15f94e626f00e488ec73417c14
Rodrigo Schaer <schaer@cora.com.br>
2022-06-22 16:22:03 -0300

    chore(config): adds graphQL apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSONS_QUERY = gql`
+  query Lessons {
+    lessons {
+      title
+      slug
+    }
+  }
+`;
+
 export const App = () => {
+  useEffect(() => {
+    client
+      .query({ query: GET_LESSONS_QUERY })
+      .then((response) => console.log(response.data));
+  });
+
   return <h1 className="text-2xl font-bold"> Rodrigo OS </h1>;
 };
